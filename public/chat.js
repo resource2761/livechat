24dfaeb1ceaf926763f352780150cd5f9f0ae663
Manish Gupta <resource@emercees.com>
2021-07-23 17:11:30 +0530

    livechat 23-07-2021

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,6 @@
-var socket=io.connect('http://localhost:3000');
+//var socket=io.connect('http://localhost:3000');
+var socket=io.connect('https://welivechat.herokuapp.com/');
+
 
 var message=document.getElementById("message");
 var button=document.getElementById("send");
